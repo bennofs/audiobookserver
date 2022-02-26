a07a2dd48e0e20fbd95a8fc23ac2bf77dd950465
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2022-02-26 21:16:16 +0100

    Fix m4b downloaded file names

--- serve.py
+++ serve.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 import re
-import zipstream
+from flask.helpers import make_response
 import os
 import os.path
-from flask import Flask, Response, url_for, send_file, safe_join, render_template
-from urllib.parse import quote
+from flask import (
+    Flask,
+    url_for,
+    send_file,
+    safe_join,
+    render_template,
+)
 
 MEDIA_ROOT = os.path.abspath(os.getenv("MEDIA_ROOT", "/tmp/audiobooks"))
-NAME_RE = re.compile(r'^[a-zA-ZäöüÄÖÜ:\- 0-9.]*$')
+NAME_RE = re.compile(r"^[a-zA-ZäöüÄÖÜ:\- 0-9.]*$")
 VALID_BOOK_CACHE = set()
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -25,34 +30,58 @@ def load_books():
             yield book
             continue
 
-        if not os.path.exists(os.path.join(MEDIA_ROOT, book, 'cover.jpg')):
+        if not os.path.exists(os.path.join(MEDIA_ROOT, book, "cover.jpg")):
             continue
 
         VALID_BOOK_CACHE.add(book)
         yield book
 
-@app.route('/')
+
+@app.route("/")
 def index():
     books = [
-        { "name": name, "download": url_for('get_zip', name=name), "cover": url_for('cover', name=name) }
+        {
+            "name": name,
+            "download": url_for("get_zip", name=name),
+            "cover": url_for("cover", name=name),
+        }
         for name in sorted(load_books())
     ]
-    return render_template('index.html', books=books)
+    return render_template("index.html", books=books)
 
-@app.route('/<name>')
+
+@app.route("/<name>")
 def get_zip(name):
     if not NAME_RE.match(name):
         return "invalid name", 404
 
     zip_name = safe_join(MEDIA_ROOT, name + ".zip")
     if os.path.exists(zip_name):
-        return send_file(zip_name, as_attachment=True, attachment_filename=os.path.basename(zip_name))
+        res = make_response(
+            send_file(
+                zip_name,
+                mimetype="application/zip",
+                as_attachment=True,
+                attachment_filename=os.path.basename(zip_name),
+            )
+        )
+        res.headers["X-Content-Type-Options"] = "nosniff"
+        return res
 
     m4b_name = safe_join(MEDIA_ROOT, name + ".m4b")
     if os.path.exists(m4b_name):
-        return send_file(m4b_name, as_attachment=True, attachment_filename=os.path.basename(m4b_name))
+        res = make_response(
+            send_file(
+                m4b_name,
+                mimetype="audio/x-m4a",
+                as_attachment=True,
+                attachment_filename=os.path.basename(m4b_name),
+            )
+        )
+        res.headers["X-Content-Type-Options"] = "nosniff"
+        return res
 
-    return 'missing file', 500
+    return "missing file", 500
 
 @app.route("/<name>.jpg")
 def cover(name):
@@ -61,5 +90,6 @@ def cover(name):
 
     return send_file(safe_join(MEDIA_ROOT, name, "cover.jpg"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
