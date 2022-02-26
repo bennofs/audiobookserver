8cef7efce0448c78151d36e51b35d1470a99ce12
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2022-02-26 21:03:10 +0100

    Fixes and m4b support

--- serve.py
+++ serve.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
+import re
+import zipstream
 import os
 import os.path
-import re
+from flask import Flask, Response, url_for, send_file, safe_join, render_template
 from urllib.parse import quote
 
-import zipstream
-from flask import Flask, Response, url_for, send_file, safe_join, render_template
-
 MEDIA_ROOT = os.path.abspath(os.getenv("MEDIA_ROOT", "/tmp/audiobooks"))
-NAME_RE = re.compile(r'^[a-zA-ZäöüÄÖÜ:\- 0-9]*$')
+NAME_RE = re.compile(r'^[a-zA-ZäöüÄÖÜ:\- 0-9.]*$')
 VALID_BOOK_CACHE = set()
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -17,6 +16,9 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 def load_books():
     for book in os.listdir(MEDIA_ROOT):
         if not NAME_RE.match(book):
+            continue
+
+        if book.endswith(".zip") or book.endswith(".m4b"):
             continue
 
         if book in VALID_BOOK_CACHE:
@@ -37,21 +39,20 @@ def index():
     ]
     return render_template('index.html', books=books)
 
-@app.route('/<name>.zip')
+@app.route('/<name>')
 def get_zip(name):
     if not NAME_RE.match(name):
         return "invalid name", 404
-    z = zipstream.ZipFile(mode='w', compression=zipstream.ZIP_STORED)
-    root = safe_join(MEDIA_ROOT, name)
-    for dirpath, dirnames, filenames in os.walk(root):
-        for filename in filenames:
-            fpath = safe_join(dirpath, filename)
-            z.write(fpath, arcname=os.path.relpath(fpath, MEDIA_ROOT))
 
-    response = Response(z, mimetype='application/zip')
-    response.headers['Content-Disposition'] = 'attachment; filename=' + quote(name) + ".zip"
-    return response
+    zip_name = safe_join(MEDIA_ROOT, name + ".zip")
+    if os.path.exists(zip_name):
+        return send_file(zip_name, as_attachment=True, attachment_filename=os.path.basename(zip_name))
 
+    m4b_name = safe_join(MEDIA_ROOT, name + ".m4b")
+    if os.path.exists(m4b_name):
+        return send_file(m4b_name, as_attachment=True, attachment_filename=os.path.basename(m4b_name))
+
+    return 'missing file', 500
 
 @app.route("/<name>.jpg")
 def cover(name):
